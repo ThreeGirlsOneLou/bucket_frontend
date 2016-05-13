b9d2527c2a04fde2c6c31eaedcd0aa9336acabe1
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-13 15:59:03 -0400

    Scaffold event listeners for auth
    Add on submit events for sign in, sign out, change password
    Modify index.js to add event handlers on document ready
    Add app-data.js to save app data to object

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// const getFormFields = require('../../../lib/get-form-fields');
+
+const authApi = require('./api.js');
+const authUi = require('./ui.js');
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', function (event) {
+    let data = getFormFields(this);
+    event.preventDefault();
+    authApi.signUp(authUi.registerSuccess, authUi.regFail, data);
+  });
+  $('#sign-in').on('submit', function(event) {
+    let data = getFormFields(this);
+    event.preventDefault();
+    authApi.signIn(authUi.signInSuccess, authUi.signInFail, data);
+  });
+  $('#sign-out').on('submit', function(event) {
+    event.preventDefault();
+    authApi.signOut(authUi.signOutSuccess, authUi.failure);
+  });
+  $('#change-password').on('submit', function(event) {
+    event.preventDefault();
+    let data = getFormFields(this);
+    console.log("change password clicked");
+    authApi.changePW(authUi.changePWSuccess, authUi.changePWFail, data);
+  });
+};
+
+module.exports = {
+  addHandlers
+};
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+// const appEvents = require('./app/events.js');
+const authEvents = require('./auth/events.js');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+// On document ready add handlers for events on page
+$(() => {
+  // appEvents.addHandlers();
+  authEvents.addHandlers();
+});
