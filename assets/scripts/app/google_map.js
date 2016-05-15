87238d81beea3f5a33c4c3717e06d140a4fbd997
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2016-05-15 16:24:24 -0400

    Add basic Google Maps API to HTML.
    Loaded Google API at bottom of HTML. Created callback in JS.

---
+++ assets/scripts/app/google_map.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function initMap() {
+   var mapDiv = document.getElementById('map');
+   var map = new google.maps.Map(mapDiv, {
+     center: {lat: 44.540, lng: -78.546},
+     zoom: 8
+   });
+ }
