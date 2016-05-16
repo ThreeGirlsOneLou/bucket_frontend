9e0332b31ab69c4ca8b54e0dc224f44bf1f20075
susandaiute <susandaiute@gmail.com>
2016-05-16 16:09:24 -0400

    Continue Flickr API work
    Begin research on error: API key invalid format

--- assets/scripts/app/flickr.js
+++ assets/scripts/app/flickr.js
@@ -1,13 +1,16 @@
 'use strict';
 
-let searchTerm = $('#search-result-name').text(place.name);
 
-const getPhotos = (success, failure, searchTerm) => {
+
+
+let searchTerm = $('#search-result-name').text();
+
+const getPhotos = (getPhotosSuccess, getPhotosFailure, searchTerm) => {
   $.ajax({
     method: 'GET',
     url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7b1259ac0fc9a1d13b2e4e76b1a3ca&tags=' + searchTerm  + '&per_page=1&format=json&nojsoncallback=1',
-  }).done(success)
-    .fail(failure);
+  }).done(getPhotosSuccess)
+    .fail(getPhotosFailure);
 };
 
 $('#signOut').on('click', function (event) {
