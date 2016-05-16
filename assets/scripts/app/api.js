008e71d3116d670c7650f26f1458557d8711a0e4
louarnos <louarnos@gmail.com>
2016-05-16 12:11:50 -0400

    Worked on Ajax
    Get user locations by query string and user location creation works
    
    May want to refactor how we pass data to addUserLocation so that we can take a
    generalized location as a parameter.

--- assets/scripts/app/api.js
+++ assets/scripts/app/api.js
@@ -1,7 +1,56 @@
 'use strict';
 
-// const app = require('../app-data.js');
+const app = require('../app-data.js');
+
+const getUserLocations = (success, failure, user_id) => {
+  $.ajax({
+    method: 'GET',
+    url: app.api + '/user_locations?user_id=' + user_id,
+    headers: {
+    "Content-Type": "application/json",
+  },
+  }).done(success)
+    .fail(failure)
+};
+
+const addUserLocation = (success, failure, user_id, name, long, lat) => {
+  $.ajax({
+    method: 'POST',
+    url: app.api + '/locations',
+    data: {
+      user: {
+        id: user_id,
+      },
+      location: {
+        name: name,
+        coords: {
+          long: long,
+          lat: lat
+        }
+      }
+    },
+    headers: {
+    "Content-Type": "application/json",
+  },
+    headers: {
+    Authorization: 'Token token=' + app.user.token,
+  },
+  }).done(success)
+    .fail(failure)
+
+};
+
+const deleteUserLocation = () => {
+
+};
+
+const updateUserLocation = () => {
+
+};
 
 module.exports = {
-
+  getUserLocations,
+  addUserLocation,
+  deleteUserLocation,
+  updateUserLocation
 };
--- assets/scripts/app/ui.js
+++ assets/scripts/app/ui.js
@@ -2,6 +2,33 @@
 
 const app = require('../app-data.js');
 
+const getLocationsSuccess = (data) => {
+  console.log('win');
+  console.log(data);
+};
+
+const getLocationsFailure = (data) => {
+  console.log('you fail');
+  console.log(data);
+
+};
+
+const addLocationSuccess = (data) => {
+  console.log('Success');
+  console.log(data);
+
+};
+
+const addLocationFailure = (data) => {
+  console.log('you fail');
+  console.log(data);
+
+};
+
 module.exports = {
+  getLocationsSuccess,
+  getLocationsFailure,
+  addLocationSuccess,
+  addLocationFailure
 
 };
